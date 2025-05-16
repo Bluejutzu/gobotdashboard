35302d0258b447825e9004cac8d72c44a0d8f2b9
bluejutzu <kalebdaniel98@gmail.com>
2025-05-16 19:31:47 +0200

    vercel is stupid

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    redirects: async () => {
-        if (process.env.NODE_ENV === "development") {
-            return [
-                {
-                    source: "/error-demo/:slug*",
-                    destination: '/404',
-                    permanent: true,
-                }
-            ];
-        } else {
-            return []
-        }
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },
